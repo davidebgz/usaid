0a24fa83af63a0568265955dd2b67b0b1b034b90
Centro de Desarrollo De Software <cds@univo.edu.sv>
2019-11-27 09:56:37 -0600

    Add project files.

---
+++ AsistenciaUsaid/AsistenciaUsaid/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AsistenciaUsaid
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<Alumnos> RegistroAlumnos { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Alumnos Alumn = new Alumnos();
+            Alumn.Nombre = ENombre.Text;
+            Alumn.Apellido = EApellido.Text;
+            Alumn.Edad = Convert.ToInt32(EEdad.Text);
+            Alumn.Especialidad = EEspecialidad.Text;
+
+            RegistroAlumnos.Add(Alumn);
+            ListViewAlumnos.ItemsSource = null;
+            ListViewAlumnos.ItemsSource = RegistroAlumnos;
+        }
+    }
+}
---
+++ AsistenciaUsaid/AsistenciaUsaid/Model/Alumnos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AsistenciaUsaid
+{
+    public class Alumnos
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+
+        private string _Especialidad;
+
+        public string Especialidad
+        {
+            get { return _Especialidad; }
+            set { _Especialidad = value.ToUpper(); }  //Para recibir datos en mayusculas
+        }
+
+    }
+}
